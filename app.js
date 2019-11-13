160e98c52d5e32a1f8987ee61f4251e1588e189b
Yahor_Pamakha <Yahor_Pamakha@epam.com>
2019-11-13 14:34:25 +0300

    add Task 1

---
+++ app.js
@@ -0,0 +1,9 @@
+import info from './config/app-config.json';
+import User from './models/User.js';
+import Product from './models/Product.js';
+
+console.log(info.name);
+
+const Yahor = new User();
+const Pizza = new Product();
+
